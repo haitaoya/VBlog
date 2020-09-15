98a5cf6d6088e8ae244fc8898b14b24ec35e96f0
Iain <958597506@qq.com>
2020-09-15 23:47:54 +0800

    add ArticleInfo Table mapper and entry class

--- src/main/java/com/iain/blog/domain/ArticleInfo.java
+++ src/main/java/com/iain/blog/domain/ArticleInfo.java
@@ -6,12 +6,12 @@ import java.util.Date;
 
 /**
  * @Classname ArticleTyp
- * @Description 文章分类实体类
+ * @Description 文章信息实体类
  * @Date 2020/9/13 9:26 下午
  * @Created by wht
  */
 @Data
-public class ArticleCategory {
+public class ArticleInfo {
     private String pkId;
     private String typNam;
     private String superTypId;
---
+++ src/main/java/com/iain/blog/mapper/ArticleInfoMapper.java
@@ -0,0 +1,12 @@
+package com.iain.blog.mapper;
+
+import com.iain.blog.domain.ArticleCategory;
+
+/**
+ * @Classname ArticleTypMapper
+ * @Description TODO
+ * @Date 2020/9/13 9:28 下午
+ * @Created by wht
+ */
+public interface ArticleInfoMapper extends BaseMapper<ArticleCategory> {
+}
